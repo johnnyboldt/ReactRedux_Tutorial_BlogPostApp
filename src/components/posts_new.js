fe03e9a553b6bf30bcd8b4d35fc6ea620242c81e
Johnny Boldt <johnny.boldt@absorblms.com>
2018-11-19 15:44:25 -0700

    Starting on Blog Posting

---
+++ src/components/posts_new.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+    render() {
+        return(
+            <div>
+                <h3>New Post</h3>
+                Title
+                <input />
+                Categories
+                <input />
+                Content
+                <input />
+            </div>
+        );
+    }
+}
+
+export default PostsNew;
